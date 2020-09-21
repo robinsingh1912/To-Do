89c25658c44db6527b88e8b68467d7ee7d042b12
Robin Singh <robinsingh1912@gmail.com>
2020-09-21 21:59:10 +0530

    added localStorage

--- js/index.js
+++ js/index.js
@@ -1,32 +1,53 @@
+$(document).ready(function () {
+  items = localStorage.getItem("todo");
+  $("#myUl").html(items);
+  deleteListener();
+  completeListner();
+});
+
 $("form").submit((e) => {
   e.preventDefault();
-  console.log($("#search").val());
+  //console.log($("#search").val());
   const value = $("#search").val();
 
   if (!value) return;
-
   $("#myUl").prepend(` <div class="input-group d-flex">
   <li class="list-group-item pl-5 flex-grow-1">${value}</li>
   <div class="input-group-append">
       <button class="btn btn-outline-danger" type="button">x</button>
   </div>
 </div>`);
+
+  deleteListener();
+  completeListner();
+  $("form").trigger("reset");
+  //$("#search").val("");
+});
+
+function updateStorage() {
+  HTML = document.getElementById("myUl").innerHTML;
+  localStorage.setItem("todo", HTML);
+}
+
+function deleteListener() {
   const close = $(".btn-outline-danger");
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       var div = this.parentElement.parentElement;
       $(div).remove();
+      updateStorage();
     };
+    updateStorage();
   }
+}
 
+function completeListner() {
   $(".d-flex")
     .unbind()
     .click((ev) => {
       if (ev.target.nodeName === "LI") {
-        console.log(ev);
         ev.target.classList.toggle("checked");
+        updateStorage();
       }
     });
-  $("form").trigger("reset");
-  //$("#search").val("");
-});
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,11 @@
-# To-do
+# Basic To-Do Application Using Jquery & localStorage
 
 ---
 
-A to do app. Using `html`, `css`, `js` & `jQuery`.
+A Simple To-Do app. Using `html`, `css`, `js` & `jQuery`.
+
+check out [Here](https://robinsingh1912.github.io/To-Do/)
+
+![screenshot](To-Do.png)
+
+> Its all save your data on `localStorage` so that you can modify it later.
